fc74d74f91cfaab8d86f1b1359cb6853d8440bdb
DuyPhuc-hust <duyphucc1011@gmail.com>
2024-12-19 17:54:53 +0700

    .

---
+++ AimsProject/src/hust/soict/cybersec/aims/screen/MainApp.java
@@ -0,0 +1,10 @@
+package hust.soict.cybersec.aims.screen;
+
+public class MainApp {
+    public static void main(String[] args) {
+    	ControllerScreen c = new ControllerScreen();
+
+        // Show the default screen
+        c.showStoreScreen();
+    }
+}
--- GUIProject/src/hust/soict/cybersec/swing/NumberGrid.java
+++ GUIProject/src/hust/soict/cybersec/swing/NumberGrid.java
@@ -1,73 +1,81 @@
 package hust.soict.cybersec.swing;
-import javax.swing.*;
-import java.awt.*;
+
+import java.awt.BorderLayout;
+import java.awt.ComponentOrientation;
+import java.awt.Container;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class NumberGrid extends JFrame {
-    private JButton[] btnNumbers; 
-    private JButton btnDelete;   
-    private JButton btnReset;    
-    private JTextField tfDisplay;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-    public NumberGrid() {
-        this.setTitle("Number Grid");
-        this.setSize(400, 400);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+public class NumberGrid extends JFrame{
+	private JButton[] btnNumbers = new JButton[10];
+	private JButton btnDelete, btnReset;
+	private JTextField tfDisplay;
+	
+	public NumberGrid() {
+		
+		tfDisplay = new JTextField();
+		tfDisplay.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+		
+		JPanel panelButtons = new JPanel(new GridLayout(4, 3));
+		addButtons(panelButtons);
+		
+		Container cp = getContentPane();
+		cp.setLayout(new BorderLayout());
+		cp.add(tfDisplay, BorderLayout.NORTH);
+		cp.add(panelButtons, BorderLayout.CENTER);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Number Grid");
+		setSize(200, 200);
+		setVisible(true);
+	}
+	
+	private class ButtonListener implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String button = e.getActionCommand();
+			if (button.charAt(0) >= '0' && button.charAt(0) <= '9') {
+				tfDisplay.setText(tfDisplay.getText() + button);
+			}else if (button.equals("DEL")) {
+				if (tfDisplay.getText().length() >= 1) {
+					tfDisplay.setText(tfDisplay.getText().substring(0, tfDisplay.getText().length()-1));
+				}
+			}else if (button.equals("C")) {
+				tfDisplay.setText("");
+			}else {
+				tfDisplay.setText(tfDisplay.getText());
+			}
+		}
+	}
+	
+	void addButtons(JPanel panelButtons) {
+		ButtonListener btnListener = new ButtonListener();
+		for (int i = 1; i<=9; i++) {
+			btnNumbers[i] = new JButton(""+i);
+			panelButtons.add(btnNumbers[i]);
+			btnNumbers[i].addActionListener(btnListener);
+		}
+		btnDelete = new JButton("DEL");
+		panelButtons.add(btnDelete);
+		btnDelete.addActionListener(btnListener);
+		
+		btnNumbers[0] = new JButton("0");
+		panelButtons.add(btnNumbers[0]);
+		btnNumbers[0].addActionListener(btnListener);
+		
+		btnReset = new JButton("C");
+		panelButtons.add(btnReset);
+		btnReset.addActionListener(btnListener);
+	}
+	
+	public static void main(String[] args) {
+		new NumberGrid();
+	}
 
-        tfDisplay = new JTextField();
-        tfDisplay.setHorizontalAlignment(JTextField.RIGHT);
-        tfDisplay.setEditable(false);
-        this.add(tfDisplay, BorderLayout.NORTH);
-
-        JPanel panelButtons = new JPanel();
-        panelButtons.setLayout(new GridLayout(4, 3)); 
-
-                btnNumbers = new JButton[10];
-        for (int i = 1; i <= 9; i++) {
-            btnNumbers[i] = new JButton(String.valueOf(i));
-            panelButtons.add(btnNumbers[i]);
-            btnNumbers[i].addActionListener(new ButtonListener()); 
-        }
-
-        btnDelete = new JButton("DEL");
-        btnNumbers[0] = new JButton("0");
-        btnReset = new JButton("C");
-
-        panelButtons.add(btnDelete);
-        panelButtons.add(btnNumbers[0]);
-        panelButtons.add(btnReset);
-
-        btnDelete.addActionListener(new ButtonListener());
-        btnNumbers[0].addActionListener(new ButtonListener());
-        btnReset.addActionListener(new ButtonListener());
-
-        this.add(panelButtons, BorderLayout.CENTER);
-
-        this.setVisible(true);
-    }
-
-    private class ButtonListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String button = e.getActionCommand();
-
-            if (button.charAt(0) >= '0' && button.charAt(0) <= '9') {
-                tfDisplay.setText(tfDisplay.getText() + button);
-            }
-            else if (button.equals("DEL")) {
-                String currentText = tfDisplay.getText();
-                if (!currentText.isEmpty()) {
-                    tfDisplay.setText(currentText.substring(0, currentText.length() - 1));
-                }
-            }
-            else if (button.equals("C")) {
-                tfDisplay.setText("");
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        new NumberGrid();
-    }
 }
